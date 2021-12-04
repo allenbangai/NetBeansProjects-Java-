26d7914e22fce2b10625e185c8fcc213cb364b6a
allebangai <allenkamadje@gmail.com>
2021-12-04 07:50:40 +0100

    creating a Complex number class of Ex_8.11

---
+++ Java How to program/Chapter8Exercises/src/chapter8exercises/ComplexNumbers.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter8exercises;
+
+/**
+ *
+ * @author DELL
+ */
+public class ComplexNumbers {
+    float realNum;
+    float imaginaryNum;
+
+    public ComplexNumbers(float realNum, float imaginaryNum) {
+        this.realNum = realNum;
+        this.imaginaryNum = imaginaryNum;
+    }
+
+    
+    
+    
+}
