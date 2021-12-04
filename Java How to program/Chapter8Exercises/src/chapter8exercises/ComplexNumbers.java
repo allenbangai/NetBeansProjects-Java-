1b802780fd1717dd671094869444cfc4a4d492e1
allenbangai <allenkamadje@gmail.com>
2021-12-04 11:41:29 +0000

    creating getter function for real and imaginary parts and creating add and substract functions of complex numbers

--- Java How to program/Chapter8Exercises/src/chapter8exercises/ComplexNumbers.java
+++ Java How to program/Chapter8Exercises/src/chapter8exercises/ComplexNumbers.java
@@ -12,11 +12,34 @@ public class ComplexNumbers {
     float realNum;
     float imaginaryNum;
 
+    public ComplexNumbers() {
+        this.realNum = 0;
+        this.imaginaryNum = 0;
+    }
+
     public ComplexNumbers(float realNum, float imaginaryNum) {
         this.realNum = realNum;
         this.imaginaryNum = imaginaryNum;
     }
 
+    public float getRealNum() {
+        return realNum;
+    }
+
+    public float getImaginaryNum() {
+        return imaginaryNum;
+    }
+    
+    public void add(ComplexNumbers complex1, ComplexNumbers complex2){
+        this.realNum = complex1.realNum + complex2.realNum;
+        this.imaginaryNum = complex1.imaginaryNum + complex2.imaginaryNum;
+    }
+    
+    public void substract(ComplexNumbers complex1, ComplexNumbers complex2){
+        this.realNum = complex1.realNum - complex2.realNum;
+        this.imaginaryNum = complex1.imaginaryNum - complex2.imaginaryNum;        
+    }
+    
     
     
     
