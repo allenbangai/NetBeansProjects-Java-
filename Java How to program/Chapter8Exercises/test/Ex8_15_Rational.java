d8524d0cd894d1fee9f950d3117b447a5f4ac3f5
allenbangai <allenkamadje@gmail.com>
2021-12-05 23:01:46 +0000

    implementation of Rational class using the main method

--- Java How to program/Chapter8Exercises/test/Ex8_15_Rational.java
+++ Java How to program/Chapter8Exercises/test/Ex8_15_Rational.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import java.lang.Integer;
 /**
  *
  * @author DELL
@@ -55,15 +55,24 @@ public class Ex8_15_Rational {
         setDenominator(denominator);        
     }
     
-    public String printfr(){
-        return Integer.toString(getNumerator()) + "/" + Integer.toString(getDenominator());
+    //...method to return fration or ratio value of rational number in string format...
+    public static String printfr(Ex8_15_Rational rational){
+        return Integer.toString(rational.getNumerator()) + "/" + Integer.toString(rational.getDenominator());
     }
     
-    public String printfl(){
-        float value = 
+    //...method to return decimal value of rational number in string format...
+    public static String printfl(Ex8_15_Rational rational){
+        //converting integer to string by typecasting
+        float value1 = rational.getNumerator();
+        float value2 = rational.getDenominator();
+        
+        return Float.toString(value1/value2);
+    } 
+    
+    //print any data to console
+    public static void println(String data){
+        System.out.println(data);
     }
-    
-    
     
     /**
      * The following methods are for addition, subtraction, division, and multiplication of two rational numbers
@@ -84,7 +93,7 @@ public class Ex8_15_Rational {
     }
     
     //...Method to subtract two rational number from tqo different instatiated object and return the subtaction...
-    public static Ex8_15_Rational subtraction(Ex8_15_Rational rational1, Ex8_15_Rational rational2){
+    public static Ex8_15_Rational subtract(Ex8_15_Rational rational1, Ex8_15_Rational rational2){
         int newDenum = rational1.getDenominator() * rational2.getDenominator();
         int newNum = rational1.getNumerator()*rational2.getDenominator() 
                 - rational2.getNumerator()*rational1.getDenominator();
@@ -108,5 +117,29 @@ public class Ex8_15_Rational {
         
         //...return quotient
         return new Ex8_15_Rational(newNum, newDenum);      
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        println(printfr(new Ex8_15_Rational(2, 4)) + " " + printfl(new Ex8_15_Rational(2, 4)));
+        
+        //...addition...
+        println("fraction value " + printfr(add(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7)) )
+            + " decimal value " + printfl(add(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7))));
+        
+        //...substraction...
+        println("fraction value " + printfr(subtract(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7)) )
+            + " decimal value " + printfl(subtract(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7))));
+        
+        //...multiplication...
+        println("fraction value " + printfr(product(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7)) )
+            + " decimal value " + printfl(product(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7))));
+        
+        //...division...
+        println("fraction value " + printfr(quotient(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7)) )
+            + " decimal value " + printfl(quotient(new Ex8_15_Rational(2, 4), new Ex8_15_Rational(5, 7))));
     }
 }
