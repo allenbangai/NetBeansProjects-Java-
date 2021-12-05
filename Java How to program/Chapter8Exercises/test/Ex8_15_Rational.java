3ed1176bfd7e61b528fe48a406ccccb2ba18a52a
allenbangai <allenkamadje@gmail.com>
2021-12-05 13:23:53 +0000

    creating the private int nominator/denominator variable, instantiating them with a contructor, and creating their getter methods

--- Java How to program/Chapter8Exercises/test/Ex8_15_Rational.java
+++ Java How to program/Chapter8Exercises/test/Ex8_15_Rational.java
@@ -8,6 +8,30 @@
  * @author DELL
  */
 public class Ex8_15_Rational {
+    private int numerator;
+    private int denominator;
+
+    public Ex8_15_Rational() {
+        numerator = 1;
+        denominator = 2;
+    }
+
+    public Ex8_15_Rational(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
     
-    
+    private int[2] simplifyRational(){
+        
+        int[] rationalNum
+        return 
+    }
 }
