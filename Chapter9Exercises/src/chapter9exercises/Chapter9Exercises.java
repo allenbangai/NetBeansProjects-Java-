0e211c58496e2c6fef99602f42adfa8af85fb1eb
allenbangai <allenkamadje@gmail.com>
2021-12-21 22:53:43 +0000

    setting of chapter 9 main function part one

--- Chapter9Exercises/src/chapter9exercises/Chapter9Exercises.java
+++ Chapter9Exercises/src/chapter9exercises/Chapter9Exercises.java
@@ -11,10 +11,22 @@ public class Chapter9Exercises {
     int num;
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Enter exercice number you which to run")
         switch(num){
             case 1:
-                default
-    
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                break;
         }
     }
     
