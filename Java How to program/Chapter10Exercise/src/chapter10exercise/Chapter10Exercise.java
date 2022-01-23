de1e1fedb9d0ba22253ddba91be6f4daaedafd1d
allenbangai <allenkamadje@gmail.com>
2022-01-23 09:54:47 +0100

    fixes

--- Java How to program/Chapter10Exercise/src/chapter10exercise/Chapter10Exercise.java
+++ Java How to program/Chapter10Exercise/src/chapter10exercise/Chapter10Exercise.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -9,12 +11,28 @@
  */
 public class Chapter10Exercise {
 
+    private static Scanner value = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int val = value.nextInt();
+        switch (val) {
+            case 1:
+                
+                break;
+            case 2:
+            break;
+
+            case 3:
+            break;
+            default:
+                break;
+        }
         System.out.println("className.methodName()");
+
     }
     
 }
