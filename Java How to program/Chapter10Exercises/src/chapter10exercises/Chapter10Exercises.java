1d5decb3ac6fb710ee2537b95645cf6dcdaa0351
allenbangai <allenkamadje@gmail.com>
2022-01-23 14:05:41 +0000

    additional implementation

--- Java How to program/Chapter10Exercises/src/chapter10exercises/Chapter10Exercises.java
+++ Java How to program/Chapter10Exercises/src/chapter10exercises/Chapter10Exercises.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -9,10 +12,39 @@
  */
 public class Chapter10Exercises {
 
+    private static Scanner value = new Scanner(System.in);
+    private static int val;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Select 1 for PolymorphysmTest");
+        val = value.nextInt();
+        switch (val) {
+            case 1:{
+                
+                break;
+            }
+                
+            case 2:{
+
+                break;
+            }
+
+            case 3:{
+
+                
+                break;
+            }
+            
+            case 4:
+                
+                break;
+            default:
+                break;
+        }
+        System.out.println("className.methodName()");
         System.err.println("How you doing today");
     }
     
