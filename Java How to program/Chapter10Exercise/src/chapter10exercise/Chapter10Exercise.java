5a1c7526939048719655d10843ca1bc705a97d3e
allenbangai <allenkamadje@gmail.com>
2022-01-23 13:57:30 +0000

    fixes and code implementation for chapter 10 exercises

--- Java How to program/Chapter10Exercise/src/chapter10exercise/Chapter10Exercise.java
+++ Java How to program/Chapter10Exercise/src/chapter10exercise/Chapter10Exercise.java
@@ -1,3 +1,5 @@
+package chapter10exercise;
+
 import java.util.Scanner;
 
 /*
@@ -12,23 +14,35 @@ import java.util.Scanner;
 public class Chapter10Exercise {
 
     private static Scanner value = new Scanner(System.in);
+    private static int val;
 
     /**
      * @param args the command line arguments
      */
     
     public static void main(String[] args) {
-        int val = value.nextInt();
+        System.out.println("Select 1 for PolymorphysmTest");
+        val = value.nextInt();
         switch (val) {
-            case 1:
+            case 1:{
                 
                 break;
-            case 2:
+            }
                 
-            break;
+            case 2:{
 
-            case 3:
-            break;
+                break;
+            }
+
+            case 3:{
+
+                
+                break;
+            }
+            
+            case 4:
+                
+                break;
             default:
                 break;
         }
