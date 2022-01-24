1e3e4454f86bbe3decc20c91d54b61a34def0625
allenbangai <allenkamadje@gmail.com>
2022-01-24 05:31:40 +0000

    commenting code in class Tpower

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/TPower.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/TPower.java
@@ -10,5 +10,33 @@ package solarsystemsizing;
  */
 
 public class TPower {
-    
+    private int numOfLoad;
+    private int powerOfLoad;
+
+    /***
+     * instantiates parameters for a load so the total number of load can be return from object
+     * S.I.U of Load is Watt(W)
+     * @param numOfLoad
+     * @param powerOfLoad 
+     */
+    public TPower(int numOfLoad, int powerOfLoad) {
+        this.numOfLoad = numOfLoad;
+        this.powerOfLoad = powerOfLoad;
+    }
+
+    public int getNumOfLoad() {
+        return numOfLoad;
+    }
+
+    public void setNumOfLoad(int numOfLoad) {
+        this.numOfLoad = numOfLoad;
+    }
+
+    public int getPowerOfLoad() {
+        return powerOfLoad;
+    }
+
+    public void setPowerOfLoad(int powerOfLoad) {
+        this.powerOfLoad = powerOfLoad;
+    }
 }
