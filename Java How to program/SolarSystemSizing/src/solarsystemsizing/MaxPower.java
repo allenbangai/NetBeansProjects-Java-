3d0e1fdf2276895f21b96be93208b6d623c792a8
allenbangai <allenkamadje@gmail.com>
2022-01-24 16:53:39 +0000

    additional implementation for class MaxPower

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/MaxPower.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/MaxPower.java
@@ -8,19 +8,39 @@ package solarsystemsizing;
  *
  * @author DELL
  */
-public class MaxPower extends Tot{
+public class MaxPower extends Total{
     private int tPower;
+    private int thours;
 
-    public MaxPower(int tPower) {
+    public MaxPower(int tPower, int thours) {
         this.tPower = tPower;
+        this.thours = thours;
     }
 
+    /**
+     * return a total same number of hours of autonomy for all loads in the local
+     * meaning all the loads will have the same number of autonomy
+     * @return 
+     */
+    public int getThours() {
+        return thours;
+    }
+
+    public void setThours(int thours) {
+        this.thours = thours;
+    }
+    
     public int gettPower() {
         return tPower;
     }
 
     public void settPower(int tPower) {
         this.tPower = tPower;
+    }
+
+    @Override
+    public double total() {
+        return super.getVal() * gettPower() * getThours();
     }
     
 }
