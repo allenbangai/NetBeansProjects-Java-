775badac09d58f1baaa04948f83ca6630637eea9
allenbangai <allenkamadje@gmail.com>
2022-01-24 04:58:12 +0000

    additional implementation for method testEmployeePay()

--- Java How to program/Chapter10Exercises/src/chapter10exercises/Chapter10Exercises.java
+++ Java How to program/Chapter10Exercises/src/chapter10exercises/Chapter10Exercises.java
@@ -26,7 +26,7 @@ public class Chapter10Exercises {
         val = value.nextInt();
         switch (val) {
             case 1:{
-                
+                testEmployeePay();
                 break;
             }
                 
@@ -51,14 +51,39 @@ public class Chapter10Exercises {
         System.err.println("How you doing today");
     }
     
-    private void TestEmployeePay(){
+    private static void testEmployeePay(){
         // create subclass objects 
         SalariedEmployees salariedEmployee = new SalariedEmployees(800.00, "John", "Smith", "111-11-1111"); 
         HourlyEmployee hourlyEmployee = new HourlyEmployee(16.75, 40, "Karen", "Price", "222-22-2222");
         CommissionEmployee commissionEmployee = new CommissionEmployee(10000, .06, "Sue", "Jones", "333-33-3333"); 
-        Base_CommissionEmployee basePlusCommissionEmployee = new Base_CommissionEmployee( 
+        Base_CommissionEmployee baseCommissionEmployee = new Base_CommissionEmployee( 
           5000, .04, 300, "Bob", "Lewis", "444-44-4444");
 
+        //create four element employee array
+        Employee[] employees = new Employee[4];
+        //initialize the four element employee array
+        employees[1] = salariedEmployee;
+        employees[2] = hourlyEmployee;
+        employees[3] = commissionEmployee;
+        employees[4] = baseCommissionEmployee;
+        
+        System.out.println("Employees process polymorphycally \n");
+        
+        //for each for processing every element in the employees array
+        for (Employee employee : employees) {
+            //invoke toString method of employee 
+            System.out.println(employee);
+            
+            //determine if element is a base_commission employee
+            if(employee instanceof Base_CommissionEmployee){
+                Base_CommissionEmployee bce = (Base_CommissionEmployee) employee;
+                bce .setBaseSalary(1.5 * bce.getBaseSalary());
+                System.out.printf("new base salary with 10%% increase is: $%,.2f%n", bce.getBaseSalary());
+            }// ...end if...
+            
+            System.out.printf("earned $%,.2f%n%n", employee.earnings());
+        }// ...end for...
+        
     }
     
 }
