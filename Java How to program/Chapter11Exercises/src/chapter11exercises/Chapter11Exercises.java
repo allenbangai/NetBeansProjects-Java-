1c43167bbf54a7b644dfd0ab95f11a07dcc31f2c
allenbangai <allenkamadje@gmail.com>
2022-02-23 19:18:04 +0100

    defining class devision with method quotient

--- Java How to program/Chapter11Exercises/src/chapter11exercises/Chapter11Exercises.java
+++ Java How to program/Chapter11Exercises/src/chapter11exercises/Chapter11Exercises.java
@@ -3,18 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package chapter11exercises;
-
+import java.util.Scanner;
 /**
  *
  * @author Kamadje Allen
  */
 public class Chapter11Exercises {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Welcome to chapter 11");
+        Scanner scanner = new Scanner(System.in);
     }
     
 }
--- Java How to program/Chapter11Exercises/src/chapter11exercises/Division.java
+++ Java How to program/Chapter11Exercises/src/chapter11exercises/Division.java
@@ -3,11 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package chapter11exercises;
-
+import java.lang.ArithmeticException;
+import java.util.InputMismatchException;
 /**
  *
  * @author Kamadje Allen
  */
 public class Division {
-    
+    public static int quotient(int numerator, int denominator){
+        throw ArithmeticException{
+            return numerator/denominator;
+        }
+    }
 }
