f62cff99e40d4bb02a4a67f3e30f93d5e06d3001
allenbangai <allenkamadje@gmail.com>
2022-02-23 19:30:32 +0100

    quotient implementation of method implementation in main

--- Java How to program/Chapter11Exercises/src/chapter11exercises/Chapter11Exercises.java
+++ Java How to program/Chapter11Exercises/src/chapter11exercises/Chapter11Exercises.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package chapter11exercises;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  *
@@ -15,6 +16,19 @@ public class Chapter11Exercises {
     public static void main(String[] args) {
         System.out.println("Welcome to chapter 11");
         Scanner scanner = new Scanner(System.in);
+
+        do{
+            try{
+                System.out.println("Please enter an integer numerator");
+                int numerator = scanner.nextInt();
+                System.out.println("Please enter an integer denominator");
+                int denominator = scanner.nextInt();
+            }catch(InputMismatchException inputMismatchException){
+
+            }catch(ArithmeticException arithmeticException){
+                
+            }
+        }while(true);
     }
     
 }
