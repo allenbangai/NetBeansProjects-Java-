a57281e0215bd4008818a5523723a09187a90972
allenbangai <allenkamadje@gmail.com>
2022-02-28 16:41:44 +0100

    documenting class UsingChainExceptions

--- Java How to program/Chapter11Exercises/src/chapter11exercises/UsingChainExceptions.java
+++ Java How to program/Chapter11Exercises/src/chapter11exercises/UsingChainExceptions.java
@@ -7,6 +7,16 @@ package chapter11exercises;
 /**
  *
  * @author Kamadje Allen
+ * 
+ * @description
+ * Sometimes a method responds to an exception by throwing a different exception type
+ * that’s specific to the current application. If a catch block throws a new exception,
+ * the original exception’s information and stack trace are lost. Earlier Java versions 
+ * provided nomechanism to wrap the original exception information with the new exception’s
+ * information to provide a complete stack trace showing where the original problem occurred.
+ * Thismade debugging such problems particularly difficult. Chained exceptions enable an
+ * exception object to maintain the complete stack-trace information from the original 
+ * exception.
  */
 public class UsingChainExceptions {
     
