af395c4f2eb78a915d5545401a833eadff8c1c65
allenbangai <allenkamadje@gmail.com>
2022-03-01 16:59:44 +0100

    declaring class panel variables and constructor taking all variables

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Panel.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Panel.java
@@ -9,5 +9,13 @@ package solarsystemsizing;
  * @author Kamadje Allen
  */
 public class Panel {
-    
+    int power;
+    int voltage;
+    int nominalVoltage;
+
+    public Panel(int power, int voltage, int nominalVoltage) {
+        this.power = power;
+        this.voltage = voltage;
+        this.nominalVoltage = nominalVoltage;
+    }
 }
