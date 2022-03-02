6a2b3b1bac3ca8dd58e609081540561f91d4fb04
allenbangai <allenkamadje@gmail.com>
2022-03-02 12:08:03 +0100

    declaring and defining first constructor

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
@@ -11,4 +11,11 @@ public class ChargeController{
     int ratedVoltage1;
     int ratedVoltage2;
     int ratedVoltage3;
+
+    public ChargeController(int minInputCurrent, int maxInputCurrent, int maxInputVoltage, int ratedVoltage1) {
+        this.minInputCurrent = minInputCurrent;
+        this.maxInputCurrent = maxInputCurrent;
+        this.maxInputVoltage = maxInputVoltage;
+        this.ratedVoltage1 = ratedVoltage1;
+    }
 }
