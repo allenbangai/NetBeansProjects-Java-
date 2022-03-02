727607c0f39430aff7940d581a694b03c21bcbd3
allenbangai <allenkamadje@gmail.com>
2022-03-02 12:27:42 +0100

    fixes: optimizing code

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
@@ -8,32 +8,13 @@ public class ChargeController{
     int minInputCurrent;
     int maxInputCurrent;
     int maxInputVoltage;
-    int ratedVoltage1;
-    int ratedVoltage2;
-    int ratedVoltage3;
+    int ratedVoltage;
 
-    public ChargeController(int minInputCurrent, int maxInputCurrent, int maxInputVoltage, int ratedVoltage1) {
+    public ChargeController(int minInputCurrent, int maxInputCurrent, int maxInputVoltage, int ratedVoltage) {
         this.minInputCurrent = minInputCurrent;
         this.maxInputCurrent = maxInputCurrent;
         this.maxInputVoltage = maxInputVoltage;
-        this.ratedVoltage1 = ratedVoltage1;
-    }
-
-    public ChargeController(int minInputCurrent, int maxInputCurrent, int maxInputVoltage, int ratedVoltage1, int ratedVoltage2) {
-        this.minInputCurrent = minInputCurrent;
-        this.maxInputCurrent = maxInputCurrent;
-        this.maxInputVoltage = maxInputVoltage;
-        this.ratedVoltage1 = ratedVoltage1;
-        this.ratedVoltage2 = ratedVoltage2;
-    }
-
-    public ChargeController(int minInputCurrent, int maxInputCurrent, int maxInputVoltage, int ratedVoltage1, int ratedVoltage2, int ratedVoltage3) {
-        this.minInputCurrent = minInputCurrent;
-        this.maxInputCurrent = maxInputCurrent;
-        this.maxInputVoltage = maxInputVoltage;
-        this.ratedVoltage1 = ratedVoltage1;
-        this.ratedVoltage2 = ratedVoltage2;
-        this.ratedVoltage3 = ratedVoltage3;
+        this.ratedVoltage = ratedVoltage;
     }
 
     public int getMinInputCurrent() {
@@ -48,15 +29,7 @@ public class ChargeController{
         return maxInputVoltage;
     }
 
-    public int getRatedVoltage1() {
-        return ratedVoltage1;
-    }
-
-    public int getRatedVoltage2() {
-        return ratedVoltage2;
-    }
-
-    public int getRatedVoltage3() {
-        return ratedVoltage3;
+    public int getRatedVoltage() {
+        return ratedVoltage;
     }
 }
