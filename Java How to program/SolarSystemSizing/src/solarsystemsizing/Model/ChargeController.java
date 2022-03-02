96ce6f1426424c0fdf6525f98d28c5c7fae2d38d
allenbangai <allenkamadje@gmail.com>
2022-03-02 12:09:27 +0100

    declaring and defining second constructor of class chargecontroller

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
@@ -18,4 +18,12 @@ public class ChargeController{
         this.maxInputVoltage = maxInputVoltage;
         this.ratedVoltage1 = ratedVoltage1;
     }
+
+    public ChargeController(int minInputCurrent, int maxInputCurrent, int maxInputVoltage, int ratedVoltage1, int ratedVoltage2) {
+        this.minInputCurrent = minInputCurrent;
+        this.maxInputCurrent = maxInputCurrent;
+        this.maxInputVoltage = maxInputVoltage;
+        this.ratedVoltage1 = ratedVoltage1;
+        this.ratedVoltage2 = ratedVoltage2;
+    }
 }
