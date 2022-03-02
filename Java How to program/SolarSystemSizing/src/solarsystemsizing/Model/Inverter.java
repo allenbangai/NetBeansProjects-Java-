69bff332ae14536fad5851b73af7cc6b94eae51f
allenbangai <allenkamadje@gmail.com>
2022-03-02 02:01:53 +0100

    changing author name

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/Inverter.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/Inverter.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package solarsystemsizing.Model;
 
 /**
  *
- * @author DELL
+ * @author Kamadje Allen
  */
 public class Inverter {
-
+    
 }
