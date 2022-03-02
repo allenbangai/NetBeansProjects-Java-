6bf48d0d4df6d99b3dbd59c6e2d6c35fecc348de
allenbangai <allenkamadje@gmail.com>
2022-03-02 12:05:47 +0100

    declaring class ChargeController field variables

---
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
@@ -0,0 +1,14 @@
+package solarsystemsizing.Model;
+
+/**
+ *
+ * @author Kamadje Allen
+ */
+public class ChargeController{
+    int minInputCurrent;
+    int maxInputCurrent;
+    int maxInputVoltage;
+    int ratedVoltage1;
+    int ratedVoltage2;
+    int ratedVoltage3;
+}
--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/SChargeController.java
+++
@@ -1,9 +0,0 @@
-package solarsystemsizing.Model;
-
-/**
- *
- * @author Kamadje Allen
- */
-public class SChargeController {
-    
-}
