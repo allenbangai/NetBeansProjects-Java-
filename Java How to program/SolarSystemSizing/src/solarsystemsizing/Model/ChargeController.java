f6b0de3289f2c5f41c68de16331e0e5a8da19254
allenbangai <allenkamadje@gmail.com>
2022-03-07 23:23:30 +0100

    documenting ChargeController.java part 2

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/ChargeController.java
@@ -14,11 +14,23 @@ public class ChargeController{
     private int ratedVoltage;
 
     /**
-     * 
+     * This constructor represents the charateristics that the objects of the class 
+     * will have when instantiated 
      * @param minInputCurrent
+     * The minimum possible current the charge controller can take in other to function
+     * properly. This current is entered in Ampere (A).
      * @param maxInputCurrent
+     * The maximum possible curent the charge controller can take in other to function 
+     * properly. This current is entered in Ampere (A).
      * @param maxInputVoltage
+     * The maximum input voltge is the voltage solar panel supply should not exceed and 
+     * must be below or the solar panel supply should not exceed.
      * @param ratedVoltage
+     * This is the output rated voltage released to the batteries in other to charge the 
+     * battery. For certain cases, the controller can have somany different types of rated 
+     * output voltage eiher due to differnt number of output ports or can varry automatically 
+     * as it adapts to the receiving voltage of the battery. In this case, we use different 
+     * contructors for describing the same obects with differnt rated output voltage. 
      */
     public ChargeController(int minInputCurrent, int maxInputCurrent, int maxInputVoltage, int ratedVoltage) {
         this.minInputCurrent = minInputCurrent;
