cf92ce814e8b27ae0d00e138c1f4bee8c8a06841
allenbangai <allenkamadje@gmail.com>
2022-03-08 23:25:14 +0100

    documenting Battery.java part 2

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/Battery.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Model/Battery.java
@@ -1,7 +1,12 @@
 package solarsystemsizing.Model;
 
 /**
- *
+ * This class Battery.java describes the characteristics of a battery used in sizing 
+ * solar systems... As we all know, a battery both has the Energy it stores in Ampere hour
+ * (Ah) and the voltage at which it is either charged or discharged measured in volts (VDC).
+ * This characteristics are used to describe an object of a battery so we could this class to 
+ * describe different types of objects of batteries. This characteristics are {@link #voltage} 
+ * and {@link #ampereHour}.
  * @author Kamadje Allen
  */
 public class Battery {
@@ -9,9 +14,13 @@ public class Battery {
     private int ampereHour;
 
     /**
-     * 
+     * This constructor descibes the only possible object that can be instantiated from the 
+     * class Battery and takes bothe the {@link #voltage} and {@link #ampereHour}.
      * @param voltage
+     * The voltage parameter here is entered in volts (VDC) and cannot be zerro.
      * @param ampereHour
+     * The ampereHour parameter here is entered in ampere hour (AH) and value cannot be 
+     * zero but must be and interger
      */
     public Battery(int voltage, int ampereHour) {
         this.voltage = voltage;
