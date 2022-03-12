5d9fb4be0ddc6af66076cc12552cb98b595c3364
allenbangai <allenkamadje@gmail.com>
2022-03-12 14:20:58 +0100

    declaring and defining getters and setters method with a constructor for all field variables of class InverterSizing.java

--- Java How to program/SolarSystemSizing/src/solarsystemsizing/Sizing/InverterSizing.java
+++ Java How to program/SolarSystemSizing/src/solarsystemsizing/Sizing/InverterSizing.java
@@ -13,4 +13,43 @@ import solarsystemsizing.Model.Inverter;
 public class InverterSizing {
     private ArrayList<Inverter> inverters = new ArrayList<>();
     private int systemMaxPower;
+
+    /**
+     * 
+     * @param inverters
+     * @param systemMaxPower
+     */
+    public InverterSizing(ArrayList<Inverter> inverters, int systemMaxPower) {
+        this.inverters = inverters;
+        this.systemMaxPower = systemMaxPower;
+    }    
+
+    /**
+     * @return ArrayList<Inverter> return the inverters
+     */
+    public ArrayList<Inverter> getInverters() {
+        return inverters;
+    }
+
+    /**
+     * @param inverters the inverters to set
+     */
+    public void setInverters(ArrayList<Inverter> inverters) {
+        this.inverters = inverters;
+    }
+
+    /**
+     * @return int return the systemMaxPower
+     */
+    public int getSystemMaxPower() {
+        return systemMaxPower;
+    }
+
+    /**
+     * @param systemMaxPower the systemMaxPower to set
+     */
+    public void setSystemMaxPower(int systemMaxPower) {
+        this.systemMaxPower = systemMaxPower;
+    }
+
 }
