6d8530d3277b1cad99b426c42ab722dd9deb02d0
allenbagai <allenkamadje@gmail.com>
2022-03-21 22:38:35 +0100

    documenting BatterySizing.java part 2 and fixes

--- SolarSystemSizing/src/solarsystemsizing/Sizing/BatterySizing.java
+++ SolarSystemSizing/src/solarsystemsizing/Sizing/BatterySizing.java
@@ -18,15 +18,17 @@ public class BatterySizing {
     private int batteryVoltage = 0;
     private int batteryAmpereHour = 0;
 
-    public BatterySizing(){
-
-    }
-
     /**
-     * 
-     * @param batteries
-     * @param stmMaxEnergy
-     * @param inverter
+     * Constructor for sizing Battery sizing
+     * @param {@link #batteries}
+     * This is a least of all posiible batteries that the class will use to set a battery from
+     * to size the sistem.
+     * @param {@link #stmMaxEnergy}
+     * The system mazimum energy because the total set of batteries energy is suppose to be equal to 
+     * or greater than the system energy consumption in hours. 
+     * @param {@link #inverter}
+     * This inverter gives use the inverter object that was used in sizing the system for reason which
+     * can be found in our algorithm.
      */
     public BatterySizing(ArrayList<Battery> batteries, int stmMaxEnergy, Inverter inverter) {
         this.batteries = batteries;
@@ -35,7 +37,9 @@ public class BatterySizing {
     }
 
     /**
-     * @return ArrayList<Battery> return the batteries
+     * Return the possible list of batteries that can be used by the user to size the system
+     * given the user choice among them.
+     * @return
      */
     public ArrayList<Battery> getBatteries() {
         ArrayList<Battery> batteryList1 = new ArrayList<>();
@@ -49,14 +53,15 @@ public class BatterySizing {
     }
 
     /**
-     * @return int return the stmMaxEnergy
+     * @return int return the max energy that willbe used per day
      */
     public int getStmMaxEnergy() {
         return stmMaxEnergy;
     }
 
     /**
-     * @return Inverter return the inverter
+     * Return the an object of the inverter used in sizing the system.
+     * @return 
      */
     private Inverter getInverter() {
         return inverter;
@@ -64,7 +69,8 @@ public class BatterySizing {
 
     /**
      * 
-     * @return {@link #getStmMaxPower()} return {@link #getInverter()}.getRatedWatt()
+     * @return {@link #getStmMaxPower()} return {@link #getInverter()}.getRatedWatt() that is the
+     * ivnerter output power rating in Watts. 
      */
     public int getStmMaxPower(){
         return getInverter().getRatedWatt();
@@ -72,26 +78,32 @@ public class BatterySizing {
 
     /**
      * 
-     * @return {@link #getStmDCVoltage()} return {@link #getInverter()}.getDCinput()
+     * @return {@link #getStmDCVoltage()} return {@link #getInverter()}.getDCinput() that is 
+     * the inverter system DC voltage used in the system.
      */
     public int getStmDCVoltage(){
         return getInverter().getDCinput();
     }
 
     /**
-     * @return int return the batteryVoltage
+     * @return int return the batteryVoltage that will be use to size the system.
      */
     public int getBatteryVoltage() {
         return findBatteryVoltage();
     }
 
     /**
-     * @param batteryVoltage the batteryVoltage to set
+     * @param {@link #batteryVoltage} the batteryVoltage to set.
      */
     public void setBatteryVoltage(int batteryVoltage) {
         this.batteryVoltage = batteryVoltage;
     }
 
+    /**
+     * Return the default battery ampereHour needed by the system. This ampereHour value can either be
+     * programmatically chosen by the algorithm or manually chosen by the user.
+     * @return
+     */
     public int getBatteryAmpereHour(){
         if(batteryAmpereHour == 0){
             for(Battery battery: getBatteries()){
@@ -114,7 +126,7 @@ public class BatterySizing {
 
     /**
      * This methd will you determine if two instance of BatterySizing are equal and return
-     * true if they are equal else return false
+     * true if they are equal else return false.
      */
     @Override
     public boolean equals(Object o) {
@@ -129,7 +141,7 @@ public class BatterySizing {
 
     /**
      * The override {@link #toString()} prints you know everything you need to know about
-     * the class BattterySizing.java
+     * the class BattterySizing.java.
      * @return
      */
     @Override
@@ -144,7 +156,7 @@ public class BatterySizing {
     }
 
     /**
-     * Returns the required battery voltage that will be used for sizing the system
+     * Returns the required battery voltage that will be used for sizing the system.
      * @return
      */
     private int findBatteryVoltage(){
@@ -157,7 +169,7 @@ public class BatterySizing {
     }
 
     /**
-     * Return total set of batteries energy in amperehour (Ah)
+     * Return total set of batteries energy in amperehour (Ah).
      * @return
      */
     public int getBatteriesEnergy_AH(){
@@ -165,7 +177,7 @@ public class BatterySizing {
     }
 
     /**
-     * Return the required number of batteries needed for sizing the system
+     * Return the required number of batteries needed for sizing the system.
      * @return
      */
     public int findBatteryNum() {
@@ -173,7 +185,7 @@ public class BatterySizing {
     }
 
     /**
-     * Return the number of series connection for batteries array connection
+     * Return the number of series connection for batteries array connection.
      * @return
      */
     public int getSeriesConnection(){
@@ -181,7 +193,7 @@ public class BatterySizing {
     }
 
     /**
-     * Return the number of parallel connections for batteries array connection
+     * Return the number of parallel connections for batteries array connection.
      * @return
      */
     public int getParallelConnection(){
