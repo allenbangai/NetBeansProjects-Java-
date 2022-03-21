b16586f7f86bd5621ff956dcf5e9938368e151f0
allenbagai <allenkamadje@gmail.com>
2022-03-21 22:49:05 +0100

    declaring and defining method isvalid(int value) with documemtation and fixes in helper.java

--- SolarSystemSizing/src/solarsystemsizing/Util/Helper.java
+++ SolarSystemSizing/src/solarsystemsizing/Util/Helper.java
@@ -6,7 +6,7 @@ public class Helper {
     } 
     
     /**
-     * Function is to return true if input value is even and false if input is odd.
+     * Method {@link #isEven(int)} is to return true if input value is even and false if input is odd.
      * @param 
      * val is of type int
      * @return 
@@ -14,6 +14,20 @@ public class Helper {
      */
     public boolean isEven(int val){
         if(val%2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Method {@link #isvalid(int)} checks if number inputed is devisible by either 2, 3 or 5
+     * and return true od condition is satisfied.
+     * @param value is of type int
+     * @return Return value can either be true or false.
+     */
+    public boolean isvalid(int value) {
+        if(value%3 == 0 || value%5 == 0){
             return true;
         }else{
             return false;
